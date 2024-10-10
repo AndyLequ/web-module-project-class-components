c8130fdb13906dc5414b27564efac787184cb741
Andy Le <andylequ@gmail.com>
2024-10-10 14:43:28 -0500

    finished

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,35 @@
 import React from 'react'
 
+
+
 export default class App extends React.Component {
+  state={
+    todos: initialTodos
+  }
+
+  addToDo = (name) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.concat({ id: getId(), completed: false, name})
+    })
+  }
+
+  toggleCompletion = id => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(td => {
+        if(id == td.id) return { ...td, completed: !td.completed}
+        return td
+      })
+    })
+  }
+
+
   render() {
     return (
       <div>
-        Todo App
+        <TodoList todos={this.state.todos} toggleCompletion={this.toggleCompletion}>
+          <Form addTodo={this.addTodo}
       </div>
     )
   }
